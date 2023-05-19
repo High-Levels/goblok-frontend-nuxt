4058bfd43c5f6e2b9b876268711fb0e2a11c0d74
riyan-eng <fbriyant11@gmail.com>
2023-05-19 16:51:35 +0700

    create: layouts, pages

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul>
+        <li>
+            <NuxtLink to="/">home</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/about">about</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/article">artikel</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/auth/login">login</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/auth/register">register</NuxtLink>
+        </li>
+    </ul>
+    <div>
+        <slot></slot>
+    </div>
+</template>
